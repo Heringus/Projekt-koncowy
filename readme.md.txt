1d0891ab292cae8bd37d9c8363b03e95073a7b75
Heringus <p.m.szlendak@gmail.com>
2017-07-25 09:41:08 +0200

    first commit

---
+++ readme.md.txt
@@ -0,0 +1,13 @@
+Projekt koncowy
+==========
+
+Projekt koncowy kurs **front-end** 
+
+Struktura katalogow
+----------------
+1. -css
+--css
+2.js
+Autor
+-----
+Pawel
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+module.exports = {
+  entry: ['whatwg-fetch', './js/zadanie01.jsx'],
+    output : {
+        filename : './js/out.js'
+    },
+    devServer: {
+      inline: true,
+      contentBase: './',
+      port: 3001
+    },
+    watch: true,
+    module : {
+        loaders : [
+            {
+                test : /\.jsx$/,
+                exclude : /node_modules/,
+                loader : 'babel-loader',
+                query : {
+                    presets : ['es2015', 'stage-2', 'react'],
+                },
+            },
+        ],
+    },
+};
